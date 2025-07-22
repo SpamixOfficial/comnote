557d96309e01c0c63c270ca413af66a77f3a4465
SpamixOfficial <spamixofficial@gmail.com>
2025-07-22 19:03:08 +0200

    Save state, will test later

--- lib/data.dart
+++ lib/data.dart
@@ -1,12 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:comnote/api.dart';
 import 'package:comnote/api/login.dart';
 import 'package:comnote/models/state.dart';
+
 import 'package:flutter/foundation.dart';
 import 'dart:developer' as developer;
 
+import 'package:result_dart/result_dart.dart';
+import 'package:path_provider/path_provider.dart';
+
 class PreferencesHandler extends ChangeNotifier {
-  final AppState state = AppState();
+  AppState state = AppState();
   final MALApi apiClient = MALApi();
+  File? stateFile;
 
   bool get loggedIn => state.login.loggedIn;
 
@@ -31,11 +39,42 @@ class PreferencesHandler extends ChangeNotifier {
     state.login.expires = authRespValue.expiresAt;
     state.login.refreshToken = authRespValue.refreshToken;
     state.login.token = authRespValue.accessToken;
-    
+
     apiClient.setLoginState(state.login);
 
     notifyListeners();
 
     return true;
+  }
+
+  Future<Result<File>> get_statefile() async {
+    if (stateFile == null) {
+      var docPath = (await getApplicationDocumentsDirectory()).path;
+      File f = File("${docPath}/state.json");
+      stateFile = f;
+    }
+
+    return Success(stateFile!); // should be safe
+  }
+
+  Future<Result<()>> load_data() async {
+    File sFile = (await get_statefile()).getOrThrow();
+    var contents = await sFile.readAsString();
+    var jsonContents = jsonDecode(contents);
+    state = AppState.fromJson(jsonContents);
+
+    if (state.login.loggedIn) {
+      apiClient.setLoginState(state.login);
+    }
+
+    return Success(());
+  }
+
+  Future<Result<()>> save_data() async {
+    File sFile = (await get_statefile()).getOrThrow();
+    var contents = state.toJson();
+
+    stateFile = await sFile.writeAsString(jsonEncode(contents));
+    return Success(());
   }
 }
